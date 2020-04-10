ec9ae540676c83f688a181accd2b13ebe52d892c
dorimedini <dorimedini@gmail.com>
2020-04-11 00:26:44 +0300

    What am I doing, really?
    Signed-off-by: dorimedini <dorimedini@gmail.com>

---
+++ Assets/Docs/Milestones.cs
@@ -0,0 +1,16 @@
+/**
+ * < V > Stuff I already did
+ * <   > Connect network layer, play with it active without terrible performance
+ * <   > Add player model and animation (ready-made, temporary)
+ * <   > Add projectile damage
+ * <   > Successfully play with 2 people concurrently, have one take damage from the other
+ * <   > Weapon model and animation (maybe make in blender? Or take ready made)
+ * <   > Movement mechanic - grapple on pillar edge and launch
+ * <   > Better projectile animation (how to launch with precision without shooting from the camera?)
+ *?<   > Better projectile model...?
+ * <   > Texture the projectile
+ * <   > Particle effects for projectile in air and on hit
+ * <   > Get non-temporary player models
+ * <   > Non-temporary weapon models
+ * <   > Make new list
+ */
