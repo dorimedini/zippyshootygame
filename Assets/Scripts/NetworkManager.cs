8c077945f00348c556c2ea8c6741f6b11c2f58dd
dorimedini <dorimedini@gmail.com>
2020-04-15 12:46:11 +0300

    Hooked up Photon
    Imported Photon. Other than that, some project settings were changed,
    and we now have a basic network script that connects to the lobby (shows
    in corner of screen)
    
    Signed-off-by: dorimedini <dorimedini@gmail.com>

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviour
+{
+    void Start()
+    {
+        Connect();
+    }
+
+    void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.NetworkClientState.ToString());
+    }
+
+}
