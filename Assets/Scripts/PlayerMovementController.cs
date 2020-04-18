7e59a78553de29f6c000ee62972d0b50f56b8ae4
dorimedini <dorimedini@gmail.com>
2020-04-18 21:51:24 +0300

    Added controller to bridge from input to animation
    Signed-off-by: dorimedini <dorimedini@gmail.com>

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerMovementController : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetLayerWeight(1, 1);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float speed = Input.GetAxis("Vertical");
+        float direction = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", speed);
+        anim.SetFloat("Direction", direction);
+    }
+}
