60e4b5c13ef8c7e73958557bfe93353c8081e16a
dorimedini <dorimedini@gmail.com>
2020-04-19 18:21:16 +0300

    Added NetworkCharacter script to sync player data
    Signed-off-by: dorimedini <dorimedini@gmail.com>

---
+++ Assets/Scripts/NetworkCharacter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkCharacter : MonoBehaviourPun, IPunObservable
+{
+    Vector3 realPosition;
+    Quaternion realRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!photonView.IsMine)
+        {
+            transform.position = Vector3.Lerp(transform.position, realPosition, 0.1f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, 0.1f);
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo message)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            realPosition = (Vector3)stream.ReceiveNext();
+            realRotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
