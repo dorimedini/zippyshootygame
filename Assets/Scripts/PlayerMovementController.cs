2810c2815e8bcd356acae1c3ce4647ebd59a2c18
dorimedini <dorimedini@gmail.com>
2020-04-19 13:08:45 +0300

    Separate Update from FixedUpdate in movement controller
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -8,6 +8,8 @@ public class PlayerMovementController : MonoBehaviour
     public float animationSpeed = 5;
 
     private Animator anim;
+    private float speed;
+    private float direction;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +19,14 @@ public class PlayerMovementController : MonoBehaviour
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        speed = Input.GetAxis("Vertical");
+        direction = Input.GetAxis("Horizontal");
+    }
+
     void FixedUpdate()
     {
-        float speed = Input.GetAxis("Vertical");
-        float direction = Input.GetAxis("Horizontal");
         anim.SetFloat("Speed", speed);
         anim.SetFloat("Direction", direction);
     }
