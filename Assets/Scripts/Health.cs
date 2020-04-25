279b989d13579ffdf11b41600c845fc7b986e358
dorimedini <dorimedini@gmail.com>
2020-04-25 13:50:22 +0300

    Player now gets flash of red when hit
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public static int maxHealth = 100;
     public HealthBar bar;
+    public Image redOverlay;
 
     private int currentHealth;
 
@@ -16,10 +18,24 @@ public class Health : MonoBehaviour
         bar.HealToMax();
     }
 
+    void Update()
+    {
+        Color c = redOverlay.color;
+        if (redOverlay.color.a > 0.01f)
+        {
+            redOverlay.color = new Color(c.r, c.g, c.b, Mathf.Max(0, c.a - Time.deltaTime));
+        }
+        else
+        {
+            redOverlay.color = new Color(c.r, c.g, c.b, 0);
+        }
+    }
+
     public void InflictDamage(int damage)
     {
         currentHealth -= damage;
         bar.TakeDamage(damage);
+        redOverlay.color = new Color(redOverlay.color.r, redOverlay.color.g, redOverlay.color.b, 0.5f);
         if (currentHealth <= 0)
             Die();
     }
