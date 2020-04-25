a572625c89b02b4bd1a0d7b3fdf801dc93f46399
dorimedini <dorimedini@gmail.com>
2020-04-25 13:27:49 +0300

    Health UI now responds to hit from projectile
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     public static int maxHealth = 100;
-    public int currentHealth;
+    public HealthBar bar;
+
+    private int currentHealth;
 
     void Start()
     {
         currentHealth = maxHealth;
+        bar.SetMaxHealth(maxHealth);
+        bar.HealToMax();
     }
 
     public void InflictDamage(int damage)
     {
         currentHealth -= damage;
+        bar.TakeDamage(damage);
         if (currentHealth <= 0)
             Die();
     }
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image greenBar;
+    public Image reductionBar;
+
+    private int maxHealth;
+    private float currentHealth, prevCurrentHealth;
+    private float waitToReduce, initialWaitToReduce;
+
+    void Start()
+    {
+        currentHealth = prevCurrentHealth = maxHealth;
+        waitToReduce = 0;
+        initialWaitToReduce = 0.75f;
+    }
+
+    void Update()
+    {
+        // When a player takes damage, immediately display the chunk of health lost with the reduction bar.
+        // Then, after a delay, start shrinking the reduction bar.
+        if (Tools.NearlyEqual(prevCurrentHealth, currentHealth, 0.01f))
+        {
+            prevCurrentHealth = currentHealth;
+            reductionBar.fillAmount = greenBar.fillAmount;
+        }
+        else if (waitToReduce > 0.01f)
+        {
+            waitToReduce -= Time.deltaTime;
+        }
+        else
+        {
+            waitToReduce = 0;
+            reductionBar.fillAmount = Mathf.Max(currentHealth / maxHealth, reductionBar.fillAmount - Time.deltaTime);
+        }
+    }
+
+    public void SetMaxHealth(int mh) { maxHealth = mh; }
+
+    public void HealToMax()
+    {
+        currentHealth = prevCurrentHealth = maxHealth;
+        greenBar.fillAmount = reductionBar.fillAmount = 1;
+    }
+
+    // Update is called once per frame
+    public void TakeDamage(int damage)
+    {
+        prevCurrentHealth = currentHealth;
+        currentHealth = Mathf.Max(currentHealth - damage, 0);
+        greenBar.fillAmount = currentHealth / maxHealth;
+        reductionBar.fillAmount = Mathf.Min(reductionBar.fillAmount, prevCurrentHealth / maxHealth);
+        waitToReduce = initialWaitToReduce;
+    }
+}
