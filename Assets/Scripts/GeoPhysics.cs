528c315f88bae92c2d82162c65ce44da27607823
dorimedini <dorimedini@gmail.com>
2020-04-26 23:06:04 +0300

    IsGrounded now stops complaining; should always find a pillar for reference
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- Assets/Scripts/GeoPhysics.cs
+++ Assets/Scripts/GeoPhysics.cs
@@ -31,20 +31,32 @@ public static class GeoPhysics
     }
     public static float DistanceFromGround(Rigidbody player, out PillarBehaviour hitPillar)
     {
+        // Find the highest pillar cllider under the player and return the distance from it
         RaycastHit rHit;
         hitPillar = null;
-        if (!Physics.Raycast(player.transform.position + player.transform.up, -player.transform.up, out rHit))
+        float highestHit = -1;
+        var hits = Physics.RaycastAll(player.transform.position + player.transform.up, -player.transform.up);
+        if (hits.Length == 0)
         {
             Debug.LogError("Nothing under player!");
             return -1;
         }
-        hitPillar = rHit.collider.gameObject.GetComponent<PillarBehaviour>();
+        foreach (var hit in hits)
+        {
+            var pillar = hit.collider.gameObject.GetComponent<PillarBehaviour>();
+            if (pillar != null && pillar.currentHeight > highestHit)
+            {
+                hitPillar = pillar;
+                highestHit = pillar.currentHeight;
+            }
+        }
         if (hitPillar == null)
         {
             // We may have hit the bounding sphere between the pillars with this raycast. If so, ground is radius away from origin.
-            if (rHit.collider.gameObject.name == "InvertableSphere(Clone)")
+            // If hits[] has no elements we've already returned
+            if (hits[0].collider.gameObject.name == "InvertableSphere(Clone)")
                 return radius - player.position.magnitude;
-            Debug.LogError(string.Format("Raycast didn't get a pillar / bounding-sphere hit! Hit {0} instead", rHit.collider.gameObject.name));
+            Debug.LogError(string.Format("Raycast didn't get a pillar / bounding-sphere hit! Hit {0} instead", hits[0].collider.gameObject.name));
             return -1;
         }
         float dist = (radius - hitPillar.currentHeight) - player.position.magnitude; // Dist of pillar surface from origin minus dist of player from origin
