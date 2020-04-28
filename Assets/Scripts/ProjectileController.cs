7ced6eed63dfdc0aba9ba5fb884abe6ac32dac4f
dorimedini <dorimedini@gmail.com>
2020-04-28 19:55:53 +0300

    Better projectile syncing
    I don't know exactly how this worked until now... maybe it was the cause
    of the null-reference error when accessing the activeProjectiles dict...
    but anyway, now projectiles only hve colliders if they were fired by the
    local player. This is implemented by making the local player instaniate
    a projectile WITH a collider, then broadcast an RPC to all other players
    to instantiate a projectile WITHOUT a collider.
    
    The local player will now get immediate response when the projectile
    hits something.
    
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -24,8 +24,12 @@ public class ProjectileController : MonoBehaviourPun
 
     public void BroadcastFireProjectile(Vector3 source, Vector3 force, string shooterId)
     {
+        // The local player instantiates a projectile with a collider, the rest don't. So, fire the RPC first, then instantiate
+        // our own copy (we'll instantiate faster than the rest anyway)
         string projectileId = RandomStrings.Generate(projectileIdLength);
-        photonView.RPC("FireProjectile", RpcTarget.All, source, force, shooterId, projectileId);
+        photonView.RPC("FireProjectile", RpcTarget.Others, source, force, shooterId, projectileId);
+        GameObject projectile = InstantiateProjectileWithoutCollider(source, force, shooterId, projectileId);
+        projectile.GetComponent<MeshCollider>().enabled = true;
     }
 
     /** We don't want to send each projectile's location updates on the network, so let's hope initial spawn location 
@@ -33,20 +37,7 @@ public class ProjectileController : MonoBehaviourPun
     [PunRPC]
     void FireProjectile(Vector3 source, Vector3 force, string shooterId, string projectileId)
     {
-        // TODO: Once again, find why these are sometimes null..
-        if (activeProjectiles == null)
-            InitActiveProjectileDict();
-        GameObject projectile = Instantiate(projectilePrefab, source, Quaternion.identity);
-        activeProjectiles[projectileId] = projectile;
-        projectile.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
-        projectile.GetComponent<Projectile>().shooterId = shooterId;
-        projectile.GetComponent<Projectile>().projectileId = projectileId;
-        // FIXME: This seems to cause a single "master" player to be the only one who enables the colliders on projectiles.
-        // FIXME: Maybe because this script isn't on a player prefab...?
-        if (photonView.IsMine)
-        {
-            projectile.GetComponent<MeshCollider>().enabled = true;
-        }
+        InstantiateProjectileWithoutCollider(source, force, shooterId, projectileId);
     }
 
     public void BroadcastDestroyProjectile(string projectileId)
@@ -61,5 +52,18 @@ public class ProjectileController : MonoBehaviourPun
         Destroy(activeProjectiles[projectileId]);
         activeProjectiles.Remove(projectileId);
         Destroy(explosion, 3f);
+    }
+
+    GameObject InstantiateProjectileWithoutCollider(Vector3 source, Vector3 force, string shooterId, string projectileId)
+    {
+        // TODO: Once again, find why these are sometimes null..
+        if (activeProjectiles == null)
+            InitActiveProjectileDict();
+        GameObject projectile = Instantiate(projectilePrefab, source, Quaternion.identity);
+        activeProjectiles[projectileId] = projectile;
+        projectile.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+        projectile.GetComponent<Projectile>().shooterId = shooterId;
+        projectile.GetComponent<Projectile>().projectileId = projectileId;
+        return projectile;
     }
 }
