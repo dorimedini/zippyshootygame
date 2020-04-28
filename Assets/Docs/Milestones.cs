ebee2937b07067887f6bbf359061de1d93e549d8
dorimedini <dorimedini@gmail.com>
2020-04-28 15:40:58 +0300

    Done with milestone
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- Assets/Docs/Milestones.cs
+++ Assets/Docs/Milestones.cs
@@ -27,7 +27,7 @@
  * < V > Projectile explosion effect - blast radius, explosion force
  * < V > Die, respawn
  * < V > Fix IsGrounded to use RaycastAll so we don't get annoying errors for finding a projectile / other player under us
- * <   > Movement mechanic - grapple to floor (simple raycast and acceleration until either a. speed goes down (collision) or b. we get close enough)
+ * < V > Movement mechanic - grapple to floor (simple raycast and acceleration until either a. speed goes down (collision) or b. we get close enough)
  * <   > Explosions should damage the enemy players in the radius, proportional to distance
  * <   > For explosions to work correctly it would be better to put colliders on all projectiles, let each player handle explosion-on-hit (and maybe even momentarily
  *       stop hit player's transform updates to give an explosion force locally?). As is, projectiles are exploding inside pillars due to delay in Destroy RPC
