153fd2ff4ce85e21222e63ce55e2d36e309019e8
dorimedini <dorimedini@gmail.com>
2020-04-30 12:40:37 +0300

    Added chainlink-hookshot, partitioned into ordered parts
    Signed-off-by: dorimedini <dorimedini@gmail.com>

---
+++ Assets/Scripts/HookshotController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookshotController : MonoBehaviour
+{
+    public GameObject[] chainLinks;
+    public GameObject hook;
+}
