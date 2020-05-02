4b6c6de885decb1cc35bb9a8dc9cb7b91db27034
dorimedini <dorimedini@gmail.com>
2020-05-02 13:19:52 +0300

    Improved OtherPlayers... method, caused indentation change above
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- Assets/Scripts/ExplosionController.cs
+++ Assets/Scripts/ExplosionController.cs
@@ -16,40 +16,41 @@ public class ExplosionController : MonoBehaviourPun
     [PunRPC]
     public void RemoteExplosion(Vector3 position, string shooterId)
     {
-        foreach (var col in OtherPlayersInExplosion(position, shooterId))
+        // Only affect the local player, provided he's not the shooter.
+        foreach (var col in OtherPlayersInExplosion(position, shooterId, true))
         {
-            // Only affect the local player, provided he's not the shooter.
-            string hitUserId = col.GetComponent<PhotonView>().Owner.UserId;
-            if (hitUserId == localUserId)
+            Rigidbody rb = col.GetComponent<Rigidbody>();
+            float dist = (rb.ClosestPointOnBounds(position) - position).magnitude;
+            if (dist <= UserDefinedConstants.explosionRadius)
             {
-                Rigidbody rb = col.GetComponent<Rigidbody>();
-                float dist = (rb.ClosestPointOnBounds(position) - position).magnitude;
-                if (dist <= UserDefinedConstants.explosionRadius)
-                {
-                    // When player is off the ground, root motion isn't applied. In case player is grounded when explosion hit,
-                    // momentarily turn off root motion until player is lifted off the ground.
-                    col.GetComponent<PlayerMovementController>().DisableRootMotionFor(0.1f);
-                    // Some force from the explosion source, and some "upward" (inward-radial) force. Must be proportional to distance
-                    Vector3 explosionForce = (UserDefinedConstants.explosionForce * (rb.position - position).normalized +
-                                                UserDefinedConstants.explosionLift * (-rb.position)
-                                             ) / (dist + 1); // In case dist is close to zero
-                    rb.AddForce(explosionForce, ForceMode.Impulse);
-                    // Do damage (AFTER adding force, so if the ragdoll replaces it it'll fly off).
-                    // I don't know what ClosestPointOnBounds returns if the point is in the collider so clamp the dist/radius ratio to [0,1]
-                    // and use that value to get the proportion of damage to deal.
-                    float damage = UserDefinedConstants.projectileHitDamage * (1 - Mathf.Clamp01(dist / UserDefinedConstants.explosionRadius));
-                    dmgCtrl.BroadcastInflictDamage(shooterId, damage, hitUserId);
-                }
+                // When player is off the ground, root motion isn't applied. In case player is grounded when explosion hit,
+                // momentarily turn off root motion until player is lifted off the ground.
+                col.GetComponent<PlayerMovementController>().DisableRootMotionFor(0.1f);
+                // Some force from the explosion source, and some "upward" (inward-radial) force. Must be proportional to distance
+                Vector3 explosionForce = (UserDefinedConstants.explosionForce * (rb.position - position).normalized +
+                                            UserDefinedConstants.explosionLift * (-rb.position)
+                                            ) / (dist + 1); // In case dist is close to zero
+                rb.AddForce(explosionForce, ForceMode.Impulse);
+                // Do damage (AFTER adding force, so if the ragdoll replaces it it'll fly off).
+                // I don't know what ClosestPointOnBounds returns if the point is in the collider so clamp the dist/radius ratio to [0,1]
+                // and use that value to get the proportion of damage to deal.
+                float damage = UserDefinedConstants.projectileHitDamage * (1 - Mathf.Clamp01(dist / UserDefinedConstants.explosionRadius));
+                dmgCtrl.BroadcastInflictDamage(shooterId, damage, col.GetComponent<PhotonView>().Owner.UserId);
             }
         }
     }
 
-    List<Collider> OtherPlayersInExplosion(Vector3 position, string shooterId)
+    List<Collider> OtherPlayersInExplosion(Vector3 position, string shooterId, bool localOnly)
     {
         List<Collider> playerCols = new List<Collider>();
         foreach (Collider col in Physics.OverlapSphere(position, UserDefinedConstants.explosionRadius))
-            if (col.GetComponent<NetworkCharacter>() != null && col.GetComponent<PhotonView>().Owner.UserId != shooterId)
+        {
+            if (col.GetComponent<NetworkCharacter>() == null)
+                continue;
+            string hitUser = col.GetComponent<PhotonView>().Owner.UserId;
+            if (hitUser != shooterId && (!localOnly || hitUser == localUserId))
                 playerCols.Add(col);
+        }
         return playerCols;
     }
 }
