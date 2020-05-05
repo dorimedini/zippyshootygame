0dceb1635e8578211f8ba5fb527981d9be2a0e5f
dorimedini <dorimedini@gmail.com>
2020-05-05 15:51:08 +0300

    Switched to different projectile (missile)
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -32,39 +32,6 @@ public class Projectile : MonoBehaviour
     {
         destroyed = false;
         InitControllers();
-        mesh = GetComponent<MeshFilter>().mesh;
-        rend = GetComponent<MeshRenderer>();
-        mesh.MarkDynamic();
-        octoTriangles = new List<int>();
-        hexTriangles = new List<int>();
-        squareTriangles = new List<int>();
-
-        // First 12*8 triangles are the 12 octogons.
-        // The next (12*5/2) * 2 triangles are the little square gaps between octogons.
-        // The remaining are the hexagons. Each hex touches 3 octogons so there are (12*5/3) hexagons
-        // with 4 triangles in each, so a total of 4*12*5/3=80.
-        // Multiply all those by 3 to get the number of vertices related to the triangles.
-        var triangles = mesh.GetTriangles(0);
-        int octs = 3 * 12 * 8;
-        int squares = 3 * 12 * 5;
-        int hexes = 3 * 4 * 12 * 5 / 3;
-        for (int t = 0; t < octs; ++t)
-            octoTriangles.Add(triangles[t]);
-        for (int t = octs; t < octs + squares; ++t)
-            squareTriangles.Add(triangles[t]);
-        for (int t = octs + squares; t < octs + squares + hexes; ++t)
-            hexTriangles.Add(triangles[t]);
-
-        mesh.subMeshCount = 3;
-        mesh.SetTriangles(octoTriangles, 0);
-        mesh.SetTriangles(squareTriangles, 1);
-        mesh.SetTriangles(hexTriangles, 2);
-
-        Material color0 = Resources.Load("Materials/projectile_blue_0", typeof(Material)) as Material;
-        Material color1 = Resources.Load("Materials/projectile_blue_1", typeof(Material)) as Material;
-        Material color2 = Resources.Load("Materials/projectile_blue_2", typeof(Material)) as Material;
-        List<Material> colorMats = new List<Material>() { color0, color1, color2 };
-        rend.materials = colorMats.ToArray();
     }
 
     void InitControllers()
